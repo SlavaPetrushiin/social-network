210b10c44cc4d7bdd627611133473f2d2f618d10
Slava <slava91petrushin@yandex.ru>
2019-12-18 21:27:29 +0300

    Updates

---
+++ precache-manifest.3dfe5a4925c835aecefaf81a60459654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa3ebb21eb27236b1e6725ba83bb6d1",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "00016cbdac54008fee3a",
+    "url": "/social-network/static/css/main.4544265f.chunk.css"
+  },
+  {
+    "revision": "ce385a87ac25280079ae",
+    "url": "/social-network/static/js/2.37613eb4.chunk.js"
+  },
+  {
+    "revision": "00016cbdac54008fee3a",
+    "url": "/social-network/static/js/main.6cb9dfc5.chunk.js"
+  },
+  {
+    "revision": "cee8e48257cfd85afb56",
+    "url": "/social-network/static/js/runtime-main.82e87acc.js"
+  },
+  {
+    "revision": "b10e29df6c764bbd6fa7172459cfc5b0",
+    "url": "/social-network/static/media/standartPhoto.b10e29df.jpg"
+  }
+]);
