58d179bea11e9d40e247489f8885f7b36a5c20d5
Slava <slava91petrushin@yandex.ru>
2019-12-29 22:00:21 +0300

    add login in header 61

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -3,12 +3,14 @@ import dialogsPageReducer from './dialogsPage-reducer';
 import profilePageReducer from './profilePage-reducer';
 import sidebarPageReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
+import authReduser from './auth-reducer';
 
 let reducers = combineReducers({
 	profilePage : profilePageReducer,
 	dialogsPage : dialogsPageReducer,
 	sidebar : sidebarPageReducer,
-	usersPage : usersReducer
+	usersPage : usersReducer,
+	auth : authReduser
 });
 
 let store = createStore(reducers);
